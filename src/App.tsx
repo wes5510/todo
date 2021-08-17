c9fbd33d6dbffa79f87592168b6959d1bfd3764a
wes5510 <lkh5510@gmail.com>
2021-08-17 22:06:40 +0900

    feat(package): add chakra-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,7 @@
+import {
+	Button,
+	ChakraProvider,
+} from '@chakra-ui/react';
 import React, { ReactElement, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -6,36 +10,39 @@ function App(): ReactElement {
 	const [count, setCount] = useState(0);
 
 	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Hello Vite + React!</p>
-				<p>
-					<button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
-						{`count is: ${count}`}
-					</button>
-				</p>
-				<p>
-					<a
-						className="App-link"
-						href="https://reactjs.org"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Learn React
-					</a>
-					{' | '}
-					<a
-						className="App-link"
-						href="https://vitejs.dev/guide/features.html"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Vite Docs
-					</a>
-				</p>
-			</header>
-		</div>
+		<ChakraProvider>
+			<Button colorScheme="blue">Chakra Button</Button>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Hello Vite + React!</p>
+					<p>
+						<button type="button" onClick={() => setCount((cnt) => cnt + 1)}>
+							{`count is: ${count}`}
+						</button>
+					</p>
+					<p>
+						<a
+							className="App-link"
+							href="https://reactjs.org"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Learn React
+						</a>
+						{' | '}
+						<a
+							className="App-link"
+							href="https://vitejs.dev/guide/features.html"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Vite Docs
+						</a>
+					</p>
+				</header>
+			</div>
+		</ChakraProvider>
 	);
 }
 
